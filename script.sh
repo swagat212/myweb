02f655000f943da56a2a4d8b7561cf0f9f568caf
EC2 Default User <ec2-user@ip-172-31-46-200.ec2.internal>
2018-08-24 12:26:38 +0000

    FINAL EDITS FOR SCRIPTS

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+ssh ec2-user@172.31.38.34 rm /home/ec2-user/tomcat/webapps/*.war
 scp ./target/*.war ec2-user@172.31.38.34:/home/ec2-user/tomcat/webapps/myweb.war
+
 ssh ec2-user@172.31.38.34 /home/ec2-user/tomcat/bin/shutdown.sh
 ssh ec2-user@172.31.38.34 /home/ec2-user/tomcat/bin/startup.sh
